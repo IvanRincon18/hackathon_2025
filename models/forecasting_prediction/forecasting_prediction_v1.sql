2760497ab97364f82754c2980828e26d521fe3fc
Ivan Rincon <ivanrincon471@gmail.com>
2025-10-26 04:52:53 +0000

    gjhvjhv

--- models/forecasting_prediction/forecasting_prediction_v1.sql
+++ models/forecasting_prediction/forecasting_prediction_v1.sql
@@ -1,6 +1,12 @@
+{{ config(
+    materialized='table',
+    schema='forecast',                 -- dataset en BigQuery
+    alias='forecasting_prediction'  -- nombre físico de la tabla
+) }}
+
 select
-    prediction_date_key as preditiction_day,
-    yhat as actual,
-    yhat_lower as lowest_case,
-    yhat_upper as greatest_case
+    prediction_date_key as prediction_date,   -- OJO: corregí el typo
+    yhat               as actual,
+    yhat_lower         as lowest_case,
+    yhat_upper         as greatest_case
 from {{ source("forecast", "predictions") }}
