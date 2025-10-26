bd27a33a3f426553be979462ee571f3501274e5a
Ivan Rincon <ivanrincon471@gmail.com>
2025-10-26 04:36:31 +0000

    solution

--- models/forecasting_prediction/forecasting_prediction_v1.sql
+++ models/forecasting_prediction/forecasting_prediction_v1.sql
@@ -1,6 +1,6 @@
-select 
-prediction_date_key as predicition_date,
-cast(round(yhat) as int64) as actual_number,
-cast(round(yhat_lower) as int64) as lowest_case,
-cast(round(yhat_upper) as int64) as greatest_case
-from {{source('forecast', 'predictions')}}
+select
+    prediction_date_key as predicition_date,
+    cast(round(yhat) as int64) as actual_number,
+    cast(round(yhat_lower) as int64) as lowest_case,
+    cast(round(yhat_upper) as int64) as greatest_case
+from {{ source("forecast", "predictions") }}
