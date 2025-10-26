f67ac9ed1562dfdeabfefd8ef21a337adbe37440
Ivan Rincon <ivanrincon471@gmail.com>
2025-10-26 05:47:26 +0000

    ddddddd

--- models/prediction_status/predicition_status_v1.sql
+++ models/prediction_status/predicition_status_v1.sql
@@ -1,6 +1,6 @@
-select 
+select
     prediction_date_key as prediction_date,
-    cast(round(yhat as int64)) as real_case,
-    cast(round(yhat_lower as int64)) as lowest_case,
-    cast(round(yhat_upper as int64)) as greatest_case
-from {{ source('forecast', 'predictions')}}
+    cast(round(yhat) as int64)         as real_case,
+    cast(round(yhat_lower) as int64)   as lowest_case,
+    cast(round(yhat_upper) as int64)   as greatest_case
+from {{ source('forecast', 'predictions') }}
